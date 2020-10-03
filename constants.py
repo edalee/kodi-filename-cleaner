6f34d1bd5b1327927c595e32f024c48d146d64fb
Edward Lee <edlee.wwp@gmail.com>
2020-10-03 23:27:31 +0200

    Separate constants into separate file

---
+++ constants.py
@@ -0,0 +1,20 @@
+import os
+from pathlib import Path
+from typing import Union
+
+
+def get_project_root() -> Union[str, bytes, os.PathLike]:
+    return Path(__file__).parent
+
+
+root: Union[str, bytes, os.PathLike] = get_project_root()
+
+
+FILE_TYPES = ['.m4v', '.mpeg', '.mpg', '.mp4', '.mpe', '.avi', '.mkv', '.mxf', '.wmv', '.ogg', '.divx', '.srt', '.sub',
+              ".ssa", ".ass", ".usf", ".ssf"]
+EXT_TO_KEEP = ['.jpg', '.png', '.vob', '.ifo', '.bup', '.sfv', '.rar', '.subs', '.idx', '.iso']
+BLACK_LIST = ['VIDEO_TS']
+SUBTITLE_EXTENSIONS = ['.sub', '.srt', ".ssa", ".ass", ".usf", ".ssf"]
+TORRENT_DATA = os.path.join(root, "names_to_clean.txt")
+CODINGS = os.path.join(root, "audio_video_information.txt")
+LANGUAGES_JSON = os.path.join(root, 'languages.json')
